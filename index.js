e1c3a5030ff2009047d8c35e3b360d239bf4c098
jamesrmathis <jrfmathis06@outlook.com>
2022-11-16 14:26:28 -0600

    removed debug statements

--- index.js
+++ index.js
@@ -15,18 +15,14 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
     let input = req.body['input']
 
-    console.log(input);
-
     let re = /\^\/\(\d+, \d+\)/g
     let badRE = /\^\/\(\)/g
 
     if (input.match(badRE)) {
-        console.log('found bad root');
         res.render(path.join(__dirname, '/views/badInput.ejs'), {'expression': input})
     }
     
     if (input.match(re)) {
-        console.log('replacing ^/ with root');
         input = input.replace(/\^\//g, 'root')
     }
 
@@ -35,11 +31,8 @@ app.post('/', (req, res) => {
         let output = eval(input)
 
         if (input.match(/root/g)) {
-            console.log('replacing root with ^/');
             input = input.replace(/root/g, '^/')
         }
-
-        console.log(output);
 
         res.render(path.join(__dirname, '/views/output.ejs'), {'expression': input, 'result': output})
     } catch (error) {
@@ -48,8 +41,6 @@ app.post('/', (req, res) => {
 })
 
 function root(num, index) {
-    console.log('taking the root');
-
     try {
         result = Math.pow(num, 1/index)
         if (isNaN(result)) throw new Error
